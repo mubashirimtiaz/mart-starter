33e79b4fd7d00c4fc00c8f9287e7dff097be8fb4
Mubashir Imtiaz <mubashir.imtiaz123@gmail.com>
2020-08-20 07:22:04 +0500

    Update Banner.js

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -4,7 +4,12 @@ import "./Banner.css";
 const Banner = () => {
   return (
     <div className="Banner">
-      <h1>Shahana Mart</h1>
+      <h1>
+        Shahana Mart{" "}
+        <span role="img" aria-labelledby="img">
+          💜
+        </span>{" "}
+      </h1>
       <p>Comming Soon!</p>
     </div>
   );
