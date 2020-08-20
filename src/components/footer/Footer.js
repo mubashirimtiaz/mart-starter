2f90f4c4cd2525da650185478d86e170de3f03d7
Mubashir Imtiaz <mubashir.imtiaz123@gmail.com>
2020-08-20 07:33:15 +0500

    Updated Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,6 +4,14 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="Footer">
+      <p className="madeBy">
+        {" "}
+        Made with{" "}
+        <span role="img" aria-label="img">
+          💖
+        </span>{" "}
+        by <span className="org"> ARM Enterprises</span> .
+      </p>
       <p> © All rights reserved by ShahanaMart 2020 </p>
     </footer>
   );
